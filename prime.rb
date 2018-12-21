c2e24b1741047889c8f2c78ab9b242e748019dcc
Nate Hunter <hunter.j.nate@gmail.com>
2018-12-21 00:56:33 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,6 @@
+# You'll be defining a method, prime?(), that takes in an integer argument and returns a boolean of whether or not that integer is a prime number.
+
 # Add  code here!
+
+def prim?(int)
+  if int %
