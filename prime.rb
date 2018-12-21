e4ee4508cfebeda17302897a422a5835afd7e533
Nate Hunter <hunter.j.nate@gmail.com>
2018-12-21 01:01:12 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,6 +4,8 @@
   #  1. What defines an integer as a prime number? Research algorithms for how to determine if a number is prime.
   #  2. How do you create a range of numbers? How do you turn a range into an array so that it can be iterated over?
 
+# A simple but slow method of checking the primality of a given number {\displaystyle n} n, called trial division, tests whether {\displaystyle n} n is a multiple of any integer between 2 and {\displaystyle {\sqrt {n}}} {\sqrt {n}}. 
+
 # Add  code here!
 
 def prim?(int)
